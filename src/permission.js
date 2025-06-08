e8f4a53fa416d1d617cd911cb2665e604fd992ef
Nan_Shu64 <1085254227@qq.com>
2025-06-08 14:08:21 +0800

    全局前置守卫

--- src/permission.js
+++ src/permission.js
@@ -8,11 +8,14 @@ router.beforeEach((to, from, next) => {
     const token = getToken()
     //没有登录且去往非登录页，强制跳转登录页
     if (!token && to.path != "/login") {
-        toast("通知","请先登录","error")
-        return next({path:"/login"})
+        toast("通知", "请先登录", "error")
+        return next({ path: "/login" })
     }
-
-
-    //   确保 next 在任何给定的导航守卫中都被严格调用一次。
+    //防止重复登录,登录且跳转登录页
+    if (token && to.path == "/login") {
+        toast("通知", "已登录，请勿重复登录", "warning")
+        return next({ path: from.path ? from.path : "/" })
+    }
+    //确保 next 在任何给定的导航守卫中都被严格调用一次。
     next()
 })
