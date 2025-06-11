3ad62856f178c0ae7055eafd0348c97afd7841d0
Nan_Shu64 <1085254227@qq.com>
2025-06-11 18:49:39 +0800

    同步路由和存储

--- src/layout/components/FTagList.vue
+++ src/layout/components/FTagList.vue
@@ -1,92 +1,90 @@
 <script setup>
 import { ref } from 'vue'
+//onBeforeRouteUpdate添加一个导航守卫，不论当前位置何时被更新都会触发。
+import { useRoute, onBeforeRouteUpdate } from 'vue-router';
+import { useCookies } from '@vueuse/integrations/useCookies';
 
 
-let tabIndex = 2
-const editableTabsValue = ref('2')
-const editableTabs = ref([
+
+
+const cookie = useCookies()
+const route = useRoute()
+
+
+const activeTab = ref(route.path)
+const tabList = ref([
     {
-        title: 'Tab 1',
-        name: '1',
-        content: 'Tab 1 content',
-    },
-    {
-        title: 'Tab 2',
-        name: '2',
-        content: 'Tab 2 content',
-    },
-    {
-        title: 'Tab 1',
-        name: '1',
-        content: 'Tab 1 content',
-    },
-    {
-        title: 'Tab 2',
-        name: '2',
-        content: 'Tab 2 content',
-    },
-    {
-        title: 'Tab 1',
-        name: '1',
-        content: 'Tab 1 content',
-    },
-    {
-        title: 'Tab 2',
-        name: '2',
-        content: 'Tab 2 content',
-    },
-    {
-        title: 'Tab 1',
-        name: '1',
-        content: 'Tab 1 content',
-    },
-    {
-        title: 'Tab 2',
-        name: '2',
-        content: 'Tab 2 content',
+        title: '后台首页',
+        path: "/"
     },
 ])
 
-const addTab = (targetName) => {
-    const newTabName = `${++tabIndex}`
-    editableTabs.value.push({
-        title: 'New Tab',
-        name: newTabName,
-        content: 'New Tab content',
+
+//添加标签导航的方法
+function addTab(tab) {
+    let noTab = tabList.value.findIndex(t => t.path == tab.path) == -1
+    if (noTab) {
+        tabList.value.push(tab)
+    }
+    cookie.set("tabList", tabList.value)
+}
+
+// 初始化标签导航列表
+function initTabList() {
+    //从cookie当中拿tabList
+    let tbs = cookie.get("tabList")
+    //如果有的话，赋值给tbs
+    if (tbs) {
+        tabList.value = tbs
+    }
+}
+//执行 
+initTabList()
+onBeforeRouteUpdate((to, from) => {
+    //点击标签，标签处于激活状态
+    activeTab.value = to.path
+    addTab({
+        title: to.meta.title,
+        path: to.path
     })
-    editableTabsValue.value = newTabName
+})
+const changeTab = (t) => {
+    //改变当前激活标签为‘t’
+    activeTab.value = t
+    router.push(t)
 }
 const removeTab = (targetName) => {
-    const tabs = editableTabs.value
-    let activeName = editableTabsValue.value
-    if (activeName === targetName) {
-        tabs.forEach((tab, index) => {
-            if (tab.name === targetName) {
-                const nextTab = tabs[index + 1] || tabs[index - 1]
-                if (nextTab) {
-                    activeName = nextTab.name
-                }
-            }
-        })
-    }
+    //点击关闭执行的方法
+    // const tabs = editableTabs.value
+    // let activeName = editableTabsValue.value
+    // if (activeName === targetName) {
+    //     tabs.forEach((tab, index) => {
+    //         if (tab.name === targetName) {
+    //             const nextTab = tabs[index + 1] || tabs[index - 1]
+    //             if (nextTab) {
+    //                 activeName = nextTab.name
+    //             }
+    //         }
+    //     })
+    // }
 
-    editableTabsValue.value = activeName
-    editableTabs.value = tabs.filter((tab) => tab.name !== targetName)
+    // editableTabsValue.value = activeName
+    // editableTabs.value = tabs.filter((tab) => tab.name !== targetName)
 }
 </script>
 <template>
     <div class="f-tag-list" :style="{ left:$store.state.asideWidth }">
 
-        <el-tabs v-model="editableTabsValue" type="card" class="flex-1" closable @tab-remove="removeTab"
-            style="min-width:100px;">
-            <el-tab-pane v-for="item in editableTabs" :key="item.name" :label="item.title"
-                :name="item.name"></el-tab-pane>
+        <el-tabs v-model="activeTab" type="card" class="flex-1" @tab-remove="removeTab" style="min-width:100px;"
+            @tab-change="changeTab">
+            <el-tab-pane v-for="item in tabList" :key="item.path" :label="item.title" :name="item.path"
+                :closable="item.path != '/'"></el-tab-pane>
         </el-tabs>
 
-        <span class="tag-btn">
+        <span class=" tag-btn">
             <el-dropdown>
                 <span class="el-dropdown-link">
-                    <el-icon class="el-icon--right">
+                    <el-icon>
                         <arrow-down />
                     </el-icon>
                 </span>
@@ -101,8 +99,8 @@ const removeTab = (targetName) => {
                 </template>
             </el-dropdown>
         </span>
-
     </div>
+    <div style="height:44px;"></div>
 </template>
 
 
@@ -118,10 +116,18 @@ const removeTab = (targetName) => {
 .tag-btn {
     @apply bg-white rounded ml-auto flex items-center justify-center px-2;
     height: 32px;
+
+}
+
+.tag-btn .el-dropdown-link:focus-visible {
+    /* 取消下拉框的悬停黑框*/
+    outline: unset;
 }
 
 :deep(.el-tabs__header) {
     @apply mb-0;
+    border: 0 !important;
+
 }
 
 :deep(.el-tabs__nav) {
