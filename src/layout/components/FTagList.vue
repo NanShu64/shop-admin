b1e81b9bd4a834f72b59dff92dc53f05b27626ef
Nan_Shu64 <1085254227@qq.com>
2025-06-11 16:04:06 +0800

    标签导航样式布局

--- src/layout/components/FTagList.vue
+++ src/layout/components/FTagList.vue
@@ -1,11 +1,153 @@
 <script setup>
+import { ref } from 'vue'
 
+
+let tabIndex = 2
+const editableTabsValue = ref('2')
+const editableTabs = ref([
+    {
+        title: 'Tab 1',
+        name: '1',
+        content: 'Tab 1 content',
+    },
+    {
+        title: 'Tab 2',
+        name: '2',
+        content: 'Tab 2 content',
+    },
+    {
+        title: 'Tab 1',
+        name: '1',
+        content: 'Tab 1 content',
+    },
+    {
+        title: 'Tab 2',
+        name: '2',
+        content: 'Tab 2 content',
+    },
+    {
+        title: 'Tab 1',
+        name: '1',
+        content: 'Tab 1 content',
+    },
+    {
+        title: 'Tab 2',
+        name: '2',
+        content: 'Tab 2 content',
+    },
+    {
+        title: 'Tab 1',
+        name: '1',
+        content: 'Tab 1 content',
+    },
+    {
+        title: 'Tab 2',
+        name: '2',
+        content: 'Tab 2 content',
+    },
+])
+
+const addTab = (targetName) => {
+    const newTabName = `${++tabIndex}`
+    editableTabs.value.push({
+        title: 'New Tab',
+        name: newTabName,
+        content: 'New Tab content',
+    })
+    editableTabsValue.value = newTabName
+}
+const removeTab = (targetName) => {
+    const tabs = editableTabs.value
+    let activeName = editableTabsValue.value
+    if (activeName === targetName) {
+        tabs.forEach((tab, index) => {
+            if (tab.name === targetName) {
+                const nextTab = tabs[index + 1] || tabs[index - 1]
+                if (nextTab) {
+                    activeName = nextTab.name
+                }
+            }
+        })
+    }
+
+    editableTabsValue.value = activeName
+    editableTabs.value = tabs.filter((tab) => tab.name !== targetName)
+}
 </script>
 <template>
-<div>标签</div>
+    <div class="f-tag-list" :style="{ left:$store.state.asideWidth }">
+
+        <el-tabs v-model="editableTabsValue" type="card" class="flex-1" closable @tab-remove="removeTab"
+            style="min-width:100px;">
+            <el-tab-pane v-for="item in editableTabs" :key="item.name" :label="item.title"
+                :name="item.name"></el-tab-pane>
+        </el-tabs>
+
+        <span class="tag-btn">
+            <el-dropdown>
+                <span class="el-dropdown-link">
+                    <el-icon class="el-icon--right">
+                        <arrow-down />
+                    </el-icon>
+                </span>
+                <template #dropdown>
+                    <el-dropdown-menu>
+                        <el-dropdown-item>Action 1</el-dropdown-item>
+                        <el-dropdown-item>Action 2</el-dropdown-item>
+                        <el-dropdown-item>Action 3</el-dropdown-item>
+                        <el-dropdown-item disabled>Action 4</el-dropdown-item>
+                        <el-dropdown-item divided>Action 5</el-dropdown-item>
+                    </el-dropdown-menu>
+                </template>
+            </el-dropdown>
+        </span>
+
+    </div>
 </template>
 
 
 <style scoped>
+.f-tag-list {
+    @apply fixed bg-gray-100 flex items-center px-2;
+    top: 64px;
+    right: 0;
+    height: 44px;
+    z-index: 100;
+}
 
+.tag-btn {
+    @apply bg-white rounded ml-auto flex items-center justify-center px-2;
+    height: 32px;
+}
+
+:deep(.el-tabs__header) {
+    @apply mb-0;
+}
+
+:deep(.el-tabs__nav) {
+    /* 边框粗细 */
+    border: 0 !important;
+}
+
+:deep(.el-tabs__item) {
+    /* 边框粗细 */
+    border: 0 !important;
+    height: 32px;
+    /* 行高为32px */
+    line-height: 32px;
+    /* 左右间距为1 */
+    @apply bg-white mx-1 rounded;
+}
+
+:deep(.el-tabs__nav-next),
+:deep(.el-tabs__nav-prev) {
+    line-height: 32px;
+    height: 32px;
+}
+
+:deep(.is-disabled) {
+    /* 鼠标指针显示为禁止 */
+    cursor: not-allowed;
+    @apply text-gray-300;
+}
 </style>
