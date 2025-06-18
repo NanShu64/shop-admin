0e10be27ddea154e8fbefe627f662790c1455e62
Nan_Shu64 <1085254227@qq.com>
2025-06-18 16:08:53 +0800

    图标下拉修改状态和增删改功能

--- src/api/rule.js
+++ src/api/rule.js
@@ -1,20 +1,20 @@
-import axios from "@/axios";
+import axios from '@/axios'
 
 export function getRuleList(page) {
     return axios.get(`/admin/rule/${page}`)
 }
 export function createRule(data) {
-    return axios.post("/admin/rule/", data)
-}
-export function deleteRule(id, data) {
-    return axios.post(`/admin/rule/${id}/delete`, data)
+    return axios.post("/admin/rule", data)
 }
 export function updateRule(id, data) {
-    return axios.post("/admin/rule/193" + id, data)
+    return axios.post("/admin/rule/" + id, data)
 }
 export function updateRuleStatus(id, status) {
     return axios.post(`/admin/rule/${id}/update_status`, {
         // 需要传一个对象
         status
     })
+}
+export function deleteRule(id) {
+    return axios.post(`/admin/rule/${id}/delete`)
 }
---
+++ src/components/IconSelect.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from 'vue'
+import * as iconList from '@element-plus/icons-vue'
+defineProps({
+    methodValue: String,
+})
+const icons = ref(Object.keys(iconList))
+
+const emit = defineEmits(["update:modelValue"])
+const handleChange = (icon) => {
+    // icon选中值 
+    emit("update:modelValue", icon)
+}
+
+</script>
+<template>
+    <div class="flex items-center">
+        <!-- 动态图标渲染-->
+        <el-icon :size="20" v-if="methodValue" class="mr-2">
+            <!-- 动态图标 -->
+            <component :is="methodValue" />
+        </el-icon>
+        <el-select fillterable :methodValue="methodValue" placeholder="请选择图标" @change="handleChange">
+            <el-option v-for=" item in icons" :key="item" :label="item" :value="item">
+                <div class="flex items-center justify-between">
+                    <el-icon>
+                        <!-- 动态图标 -->
+                        <component :is="item" />
+                    </el-icon>
+                    <span class="text-gray-500">{{ item }}</span>
+                </div>
+            </el-option>
+        </el-select>
+    </div>
+</template>
+
+
+<style scoped></style>
