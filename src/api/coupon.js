a5ab675b87d5dbf4d9ebe4f048ffc3b818903ce0
Nan_Shu64 <1085254227@qq.com>
2025-06-19 22:24:23 +0800

    优惠券模块布局实现

---
+++ src/api/coupon.js
@@ -0,0 +1,19 @@
+import axios from "@/axios";
+
+export function getCouponList(page) {
+    return axios.get(`/admin/coupon/${page}`)
+}
+export function createCoupon(data) {
+    return axios.post("/admin/coupon", data)
+}
+export function deleteCoupon(id) {
+    return axios.post(`/admin/coupon/${id}/delete`)
+}
+export function updateCoupon(id, data) {
+    return axios.post("/admin/coupon/" + id, data)
+}
+export function updateCouponStatus(id, data) {
+    return axios.post("/admin/coupon/${id}/updata_status", {
+        status: 0
+    })
+}
